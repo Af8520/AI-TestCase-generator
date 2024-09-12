d5f6c376f53d294a69fba69025af20a872f1856e
Af8520 <alonfiban@gmail.com>
2024-09-12 20:05:31 +0300

    Initial commit

---
+++ data.py
@@ -0,0 +1,7 @@
+class Credentials:
+    API_END_POINT = ""
+    API_KEY = "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"
+    ASSISTANT_ID = "asst_JDtUqJXfuXqbYagyUpTlf8cA"
+
+    assistant_id = "asst_uEdU1nMOyRniqfRYnCNRa9gv"
+    thread_id = "thread_QaveOqdFz4j7BZJgxUN8S2m3"
